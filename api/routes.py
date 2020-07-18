5dd73905c09c7dd58fcae60d79a0dafb49de6897
Jose Andres Moya Porras <jose.91cr@gmail.com>
2020-07-17 20:00:59 -0600

    adding translation api and js

--- api/routes.py
+++ api/routes.py
@@ -4,4 +4,17 @@ api = Blueprint('api', '__name__')
 
 @api.route('/')
 def index():
+<<<<<<< Updated upstream
     return "Hello World"
+=======
+    return "Hello World"
+
+@api.route('/translate')
+def translate():
+    sourceText = request.args.get('sourceText')
+    targetLanguage = request.args.get('targetLanguage')
+    translator = Translator()
+    translation = translator.translate(sourceText,dest=targetLanguage)
+    translation = {"text":translation.text}
+    return json.dumps(translation)
+>>>>>>> Stashed changes
